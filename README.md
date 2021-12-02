e6b2de9dfefc0c7b39c506d79a5e20c186965008
kaleb-garity <82844817+kaleb-garity@users.noreply.github.com>
2021-12-01 20:12:42 -0800

    Updated TODO

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # to-do-organizer
-Organizes a to do list based on time and importance. Sends updates to phone to stay current. 
+Organizes a to do list based on time and importance. Sends updates to phone to stay current.
+
+Pseudo:
+
+Basic UI that takes in an input with 2 parameters, importance 1-5 and date due/expected to be done
+Easily add + delete items from list + save updates to a file
+Customizable notification settings
+Notify when due dates are coming up (off, 1 day, 2 days, 1 week)
+Auto sorts list on priority + due date
+Include expected completion time?
+Store in file "Clean room 3 10/22/21
---
+++ to-do/to-do.py
@@ -0,0 +1,20 @@
+# 10/21/21
+# To Do program: Organizes a to do list based on time and importance. Sends updates on due dates to a phone.
+
+class Task:
+    def __init__(self, name, importance, date):
+        self.name = name
+        self.importance = importance
+        self.date = date
+
+container = ""
+exampleTask = Task("Clean", 10, "10/24/21")
+
+with open("to-do-list.txt", "a+") as file:
+    container = str(exampleTask.name) + " " + str(exampleTask.importance) + " " + str(exampleTask.date)
+    file.write(container)
+    file.write("\n")
+
+with open('to-do-list.txt', 'r+') as file:
+    print(file.read())
+
